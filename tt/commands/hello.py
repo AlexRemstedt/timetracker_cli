1fa4e984bac92a29f53bde79a7d8dc66b9ee01f7
AlexRemstedt <alex@remstedt.com>
2021-06-28 13:16:58 +0200

    task tracker finished

--- tt/commands/hello.py
+++ tt/commands/hello.py
@@ -2,37 +2,34 @@
 import click
 from datetime import datetime
 
-data_location = 'tt/data/worktracker'
 today = datetime.now()
+data_location = f'tt/data/{today.day}{today.month}{today.year}'
 
 
 def get_day():
     """Get current day."""
-    return f"{today.day}/{today.month}/{today.year}"
+    return f"{today.strftime('%d/%m/%Y')}"
 
 
 def get_start_time():
     """Get the time at which the day starts."""
-    return f"{today.hour}:{today.minute}"
+    return f"{today.strftime('%H:%M:%S')}"
 
 
 @click.command()
 def cli():
     """Startup command in the morning."""
     # header of each day
-    header = f"Date: {get_day()}"
+    header = f"Date: {today.strftime('%A')} {get_day()}"
     
-    # check if header has been placed
-    with open(data_location, 'r') as read_data:
-        check = read_data.read()
-        check = check.split('\n')
-    if header in check:
-        click.echo("Goodmorning, again")
-    
-    # create new header
-    else:
+    try:
+        open(data_location, 'r')
+        click.echo("Goodmorning, again!")
+    except FileNotFoundError:
         click.echo("Goodmorning")
-        with open(data_location, 'a') as data:
+        with open(f'{data_location}', 'w') as data:
             data.write(f"""{header}
-Wake up time: {get_start_time()}
-""")
+Wake up time - {get_start_time()}
+
+  Time                       Task
+-----------------------------------""")
--- tt/commands/task.py
+++ tt/commands/task.py
@@ -1,9 +1,57 @@
 """Add task to timetracker."""
+from fileinput import close
 import click
 from datetime import datetime
-from . import hello
+
+today = datetime.now()
+data_location = f'tt/data/{today.day}{today.month}{today.year}'
 
 
-def cli():
+class Task:
+    """Create task."""
+    
+    def __init__(self, task):
+        self.task = task
+        self.time = f"{datetime.now()}"
+    
+
+def file_exist(file):
+    """Check if file exists."""
+    try:
+        open(file, 'r')
+        close()
+    except FileNotFoundError:
+        return False
+    return True
+   
+ 
+@click.group()
+@click.option('-t', "--task", type=str, help="Task about to be started")
+@click.pass_context
+def cli(ctx, task):
     """Add task to time tracker."""
-    pass
+    ctx.obj = Task(task)
+
+
+@cli.command()
+@click.pass_context
+def add(ctx):
+    """Add task."""
+    if file_exist(data_location):
+        with open(data_location, 'a') as f:
+            f.write(f"""
+s,{ctx.obj.time},{ctx.obj.task}""")
+    else:
+        click.echo("Say hello first!")
+
+
+@cli.command()
+@click.pass_context
+def end(ctx):
+    """End current task."""
+    if file_exist(data_location):
+        with open(data_location, 'a') as f:
+            f.write(f"""
+e,{ctx.obj.time},{ctx.obj.task}""")
+    else:
+        click.echo("Say hello first!")
